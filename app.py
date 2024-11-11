2124316eecb7eaf336413f2e3adf787c28f50bfb
valdemir <valdemirsuportetecnico@gmail.com>
2024-11-11 17:30:19 -0300

    fix: formatting according to flake8

--- app.py
+++ app.py
@@ -4,11 +4,14 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
+
 class HelloWorld(Resource):
     def get(self):
         return {'hello': 'world'}
 
+
 api.add_resource(HelloWorld, '/')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
